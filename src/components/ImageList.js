21bbdd98638c37ea66a6f2bf9ee37e4e7e3bf948
LAPTOP-EGDAPS3Q\reddy <reddyvasipalli98@outlook.com>
2020-04-27 18:15:06 -0500

    Image search UI grid completed

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) =>{
-    const imageList = props.images.map(({id, description, urls}) => {
-        return <img key={id} src={urls.regular} alt={description}/>
+    const imageList = props.images.map((image) => {
+        return <ImageCard key = {image.id}image={image}/>
     });
-return <div>{imageList}</div>
+return <div className="image-list">{imageList}</div>
 };
 
 export default ImageList;
